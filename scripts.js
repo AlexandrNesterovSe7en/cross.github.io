9b0a94da23de5562e48c2a192e387ecebc0c5bbc
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2023-04-21 10:12:22 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let items = [];
 let helpMassive = [];
+const INPUTS_TEXT = document.querySelectorAll('input[type="text"]')
 const FIELDS = {
     '1': {
         name: 'сульфатация',
@@ -193,43 +194,78 @@ let cross = new CrossInit(items);
 
 cross.init()
 
-
+let i = 0;
 for (let item in FIELDS) {
-    
+    INPUTS_TEXT[i].setAttribute('maxlength', FIELDS[item].name.length)
+    i++
     if ( FIELDS[item]?.name ) {
         setRow(FIELDS[item], item)
         setColumn(FIELDS[item], item)
     }
 }
 
-
-function setRow(field, count) {
+function setRow(field, count, value='', flag=true) {
     let {name, startPos:{x, y}, direction} = field;
     if ( direction === 'row') {
         items[y][x - 1].innerHTML = count;
         for ( let i = 0; i < name.length; i++) {
-
+            if (value.length === i && !flag){
+                break;
+            } 
             let div = items[y][x++];
             
+            if (flag) {
+                div.classList.add('focusItem');
+                continue;
+            }
 
-            div.classList.add('focusItem')
-            div.innerHTML = name[i];
+            div.innerHTML = value[i];
         }
     }
 }
 
-function setColumn(field, count) {
+function setColumn(field, count, value='', flag=true) {
     let {name, startPos:{x, y}, direction} = field;
         
     if ( direction === 'column') {
         items[y - 1][x].innerHTML = count;
         for ( let i = 0; i < name.length; i++) {
+            if (value.length === i && !flag){
+                break;
+            } 
 
             let div = items[y++][x];
             
+            if (flag) {
+                div.classList.add('focusItem');
+                continue;
+            }
 
-            div.classList.add('focusItem')
-            div.innerHTML = name[i];
+            div.innerHTML = value[i];
+
+                       
         }
-    }
+    } 
+
+
 }
+
+const containerRoot = document.querySelector('.container-root')
+
+containerRoot.addEventListener('input', (e) => {
+    const input = e.target
+    if (input.hasAttributes('type', 'text')) {
+        const count = input.dataset.id;
+        const field = FIELDS[count];
+
+        input.dataset.answer = input.value;
+
+        setRow(field, count, input.value, false);
+        setColumn(field, count, input.value, false)
+    }
+})
+
+document.querySelector('button').addEventListener('click', (e) => {
+    const btn = e.target;
+    btn.classList.add('active')
+})
